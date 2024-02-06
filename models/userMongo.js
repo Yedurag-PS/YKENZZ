3d882b495b33b88aea03a63e1318d68a9f47cba4
Yedurag-PS <yedurag10yjr@gmail.com>
2024-02-07 00:45:14 +0530

    first commit

---
+++ models/userMongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const usrsignupSchema = new mongoose.Schema({
+    Username:{
+        type:String,
+        require:true
+    },
+    Email:{
+        type:String,
+        require:true
+    },
+    Password:{
+        type:String,
+        require:true
+    },
+    Mobilenumber:{
+        type:Number,
+        require:true
+    },
+    isListed:{
+        type:Boolean,
+        require:true
+    },
+    Wallet :{    
+        type:Number,
+        require:true
+    },
+    refferal:{
+        type:String
+    }
+})
+
+module.exports = mongoose.model("userdatas",usrsignupSchema)     
